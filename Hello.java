1089da1a680b2b2cd7a3336dc49726e8d438471a
Brian Hauck <brieker101@gmail.com>
2019-11-15 21:58:27 -0500

    Adding bad comment

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello
  public static void main( String argv[] )
  {//Default is "World"
  String name = "World";
+	//This is a bad comment. We want to revert it.
  if ( argv.length != 0 )
  {
  name = argv[0];
